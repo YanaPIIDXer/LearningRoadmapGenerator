69758aa1817eae779070b56f207bf3befbb8784b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-20 04:23:05 +0900

    LINE LIFFのログイン処理

--- generator/src/App.vue
+++ generator/src/App.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import liff from "@line/liff";
+
+onMounted(() => {
+  liff.init({
+    liffId: import.meta.env.VITE_APP_LINE_LIFF_ID,
+    withLoginOnExternalBrowser: true,
+  }, () => {
+    console.log("Init LIFF Success!");
+  }, () => {
+    console.error("Init LIFF Error...");
+  });
+});
 </script>
 
 <template lang="pug">
