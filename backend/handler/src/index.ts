f8fec56fb290d6602db43159e649a87d93957e5e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-20 06:00:19 +0900

    とりあえずローカルでのテスト環境

--- backend/handler/src/index.ts
+++ backend/handler/src/index.ts
@@ -1,9 +1,7 @@
 import { APIGatewayEvent , APIGatewayProxyResult } from "aws-lambda";
 
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-  console.log(JSON.stringify(event.body ?? "{}"));
-
-  return {    
+  return {
     statusCode: 200,
     body: JSON.stringify({
       message: "hello",
