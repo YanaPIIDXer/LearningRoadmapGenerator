aaa1af19ed1427103a50f29f4da07d1e8f0d4441
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-20 07:33:33 +0900

    LINE LIFFの方に誘導するURLをPOSTするようにした

--- backend/handler/src/handler/handle/Handler.ts
+++ backend/handler/src/handler/handle/Handler.ts
@@ -1,13 +1,23 @@
 import { APIGatewayEvent , APIGatewayProxyResult } from "aws-lambda";
 import { lineApi } from "../../line";
+import type { WebhookEvent, MessageEvent, TextMessage } from "@line/bot-sdk";
 
 /**
  * LINEのWebHookハンドラ
  */
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Invalid request body.",
+      }),
+    }
+  }
+  
   // シグネチャ検証
   const signature = event.headers["X-Line-Signature"] ?? "";
-  if (!lineApi.verifySignature(event.body ?? "", signature)) {
+  if (!lineApi.verifySignature(event.body, signature)) {
     return {
       statusCode: 403,
       body: JSON.stringify({
@@ -15,6 +25,23 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
       }),
     }
   }
+
+  // LIFFに誘導
+  const body = JSON.parse(event.body);
+  (body.events as WebhookEvent[]).forEach(ev => {
+    if (ev.type !== "message") { return; }
+
+    const msgEv = ev as MessageEvent;
+    if (msgEv.message.type !== "text" || !msgEv.source.userId) { return; }
+    const msg = msgEv.message as TextMessage;
+  
+    // 学びたい分野が発言されるので、LIFFの方に誘導するURLを生成する
+    const params = new URLSearchParams({
+      field: msg.text,
+      userId: msgEv.source.userId,
+    });
+    lineApi.postMessage(msgEv.source.userId, "https://learning-roadmap-generator.web.app?" + params.toString());
+  });
   
   return {
     statusCode: 200,
