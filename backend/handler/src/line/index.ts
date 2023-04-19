f2f181cbe975b8cf3db83717fd324a4d19cd6b9b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-20 07:55:56 +0900

    環境変数にbooleanを突っ込んだつもりがstringとして扱われていた不具合を修正

--- backend/handler/src/line/index.ts
+++ backend/handler/src/line/index.ts
@@ -4,6 +4,7 @@ import { LineApi } from "./LineApi";
 
 // 環境に合わせてモックと実際のオブジェクトを入れ替える
 export const constructLineApi = () => {
-  const lineApi: ILineApi = process.env.EXEC_LOCAL ? new ApiMock() : new LineApi();
+  const isExecLocal = process.env.EXEC_LOCAL?.toLowerCase() === "true";
+  const lineApi: ILineApi = isExecLocal ? new ApiMock() : new LineApi();
   return lineApi;
 }
