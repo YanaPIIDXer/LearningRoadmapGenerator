563824de71550fcabae80d45c301f050e5bda3fc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-20 03:46:03 +0900

    LINE Liffへの乗り換えに伴い一旦撤去とパッケージ整理

--- generator/src/App.vue
+++ generator/src/App.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts">
-import RoadmapImage from "./components/RoadmapImage.vue";
-import sample from "./assets/sample.json";
-
-defineExpose({
-  RoadmapImage,
-  sample,
-});
 </script>
 
 <template lang="pug">
-.root
-  RoadmapImage(:item="sample")
 </template>
 
 <style lang="sass" scoped>
