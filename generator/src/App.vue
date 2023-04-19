d99b6864878ed2f8c39c3cc240c307e3e8663a0b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-19 21:47:00 +0900

    とりあえずサンプル Componentを実装する下準備

--- generator/src/App.vue
+++ generator/src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import sample from "@/assets/sample.json";
+console.log(sample);
 </script>
 
 <template lang="pug">
+.root
+  span {{ sample.field }}
 </template>
 
 <style lang="sass" scoped>
