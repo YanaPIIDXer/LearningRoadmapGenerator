737749e9667931a80ff4f107700d67cc96685e71
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-19 22:03:00 +0900

    defineExposeを使うようにした

--- generator/src/components/RoadmapImage.vue
+++ generator/src/components/RoadmapImage.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { RoadmapResponse } from "@/interfaces/Roadmap";
-import { toRef, type Ref } from "vue";
+import { toRef, type Ref, defineExpose } from "vue";
 
 interface Props {
   item: RoadmapResponse;
@@ -8,6 +8,10 @@ interface Props {
 const props = defineProps<Props>();
 
 const item: Ref<RoadmapResponse> = toRef(props, "item");
+
+defineExpose({
+  item,
+});
 </script>
 
 <template lang="pug">
