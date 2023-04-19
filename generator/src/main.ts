bde548aac50a991dec812c6aa925189193ea941e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-19 20:25:32 +0900

    ・不要なものを撤去 ・PrimeVueの準備

--- generator/src/main.ts
+++ generator/src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './assets/main.css'
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.mount("#app");
