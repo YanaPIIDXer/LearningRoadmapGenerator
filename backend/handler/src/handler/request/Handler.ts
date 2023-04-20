eb63a6cdb6f7aadb53a55c8dd453039221a3bd03
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-20 10:26:11 +0900

    プロンプト取得

--- backend/handler/src/handler/request/Handler.ts
+++ backend/handler/src/handler/request/Handler.ts
@@ -1,11 +1,30 @@
 import { APIGatewayEvent , APIGatewayProxyResult } from "aws-lambda";
+import axios from "axios";
+import openai from "openai";
 
 /**
  * リクエストに対するハンドラ
  */
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   const body = JSON.parse(event.body ?? "{}");
-  console.log("Field", body.field);
+
+  const field = body.field;
+  const choices = body.choices;
+
+  try {
+    const conn = axios.create({
+      baseURL: process.env.FRONTEND_ORIGIN_URL,
+    });
+    const promptResponse = await conn.get("/templates/prompt.txt");
+    const prompt: string = promptResponse.data;
+    console.log(prompt);
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }),
+    }
+  }
   
   return {
     statusCode: 200,
